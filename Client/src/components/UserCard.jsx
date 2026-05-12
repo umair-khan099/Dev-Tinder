1a3b1441f7cc70ab75e00d7e9e49efa17a3a9c82
Umair khan <ukkhan5476@gmail.com>
2026-05-12 23:01:24 +0530

    feat: add UserCard component for displaying user profiles

---
+++ Client/src/components/UserCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const UserCard = ({ user, setNext }) => {
+  console.log(user);
+  return (
+    <div className="flex justify-center items-center min-h-[600px]  text-white px-4">
+      <div className="bg-zinc-900 w-full max-w-sm rounded-2xl overflow-hidden shadow-2xl border border-zinc-800">
+        <img
+          src={user.photoUrl}
+          alt="profile"
+          className="w-full h-80 object-cover"
+        />
+
+        <div className="p-5">
+          <div className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold">
+              {user.firstName}, {user.age}
+            </h1>
+
+            <span className="text-sm text-zinc-400 capitalize">
+              {user.gender}
+            </span>
+          </div>
+
+          <p className="text-zinc-300 mt-3 text-sm leading-relaxed">
+            {user.about}
+          </p>
+
+          <div className="flex flex-wrap gap-2 mt-4">
+            {user.skills.map((skill, index) => (
+              <span
+                key={index}
+                className="bg-white text-black text-xs px-3 py-1 rounded-full font-medium"
+              >
+                {skill}
+              </span>
+            ))}
+          </div>
+
+          <div className="flex gap-4 mt-6">
+            <button
+              onClick={() => setNext((prev) => prev + 1)}
+              className="w-1/2 border border-white py-2 rounded-xl font-semibold hover:bg-red-800 hover:text-white transition"
+            >
+              Ignore
+            </button>
+
+            <button
+              onClick={() => setNext((prev) => prev + 1)}
+              className="w-1/2 border border-white text-white py-2 rounded-xl font-semibold hover:bg-green-300 transition"
+            >
+              Interested
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;

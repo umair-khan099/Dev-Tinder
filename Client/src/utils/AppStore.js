c431611d1bde096fec4ac277617fc58b78cbb90f
Umair khan <ukkhan5476@gmail.com>
2026-05-12 23:01:04 +0530

    feat: add feed reducer to app store configuration

--- Client/src/utils/AppStore.js
+++ Client/src/utils/AppStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
+import feedReducer from "./feedSlice";
 const appStore = configureStore({
   reducer: {
     user: useReducer,
+    feed: feedReducer,
   },
 });
 

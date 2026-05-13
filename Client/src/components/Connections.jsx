46a3ee0d7f6a7a675fb4cd8484d8394e654fa060
Umair khan <ukkhan5476@gmail.com>
2026-05-13 16:27:13 +0530

    feat: implement fetchConnections function to retrieve user connections from API

--- Client/src/components/Connections.jsx
+++ Client/src/components/Connections.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addConnections } from "../utils/connectionsSlice";
 
 const Connections = () => {
+  const dispatch = useDispatch();
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res.data.data));
+      console.log(res.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
   return (
     <div>
       <h1>connections</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Connections
+export default Connections;

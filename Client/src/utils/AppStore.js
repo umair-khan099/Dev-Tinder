5b02be6beb06577ab42bdb744fd93307b5753f9c
Umair khan <ukkhan5476@gmail.com>
2026-05-13 16:26:37 +0530

    feat: add connectionsSlice and integrate into appStore for managing user connections

--- Client/src/utils/AppStore.js
+++ Client/src/utils/AppStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import connetionsReducer from "./connectionsSlice";
 const appStore = configureStore({
   reducer: {
     user: useReducer,
     feed: feedReducer,
+    connections: connetionsReducer,
   },
 });
 

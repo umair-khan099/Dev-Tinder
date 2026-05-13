836f50de5454c046a312f981db40392fd1525845
Umair khan <ukkhan5476@gmail.com>
2026-05-14 04:14:56 +0530

    feat: add requestsSlice for managing user requests and integrate into appStore

--- Client/src/utils/AppStore.js
+++ Client/src/utils/AppStore.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
 import feedReducer from "./feedSlice";
 import connetionsReducer from "./connectionsSlice";
+import requestsReducer from "./requests";
 const appStore = configureStore({
   reducer: {
     user: useReducer,
     feed: feedReducer,
     connections: connetionsReducer,
+    requests: requestsReducer,
   },
 });
 
---
+++ Client/src/utils/requests.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const requestsSlice = createSlice({
+  name: "requests",
+  initialState: null,
+  reducers: {
+    addRequests: (state, action) => action.payload,
+    removeRequests: (state, action) => null,
+  },
+});
+
+export const { addRequests, removeRequests } = requestsSlice.actions;
+export default requestsSlice.reducer;
